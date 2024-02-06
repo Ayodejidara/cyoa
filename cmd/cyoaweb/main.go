e296bbb4726391a80d302dae907369bd70c83963
Ojo Ayodeji <degzie24@gmail.com>
2024-02-06 22:48:39 +0100

    first commit

---
+++ cmd/cyoaweb/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"cyoa"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
+	fileName := flag.String("file", "gopher.json", "the JSON file with the cyoa story")
+	flag.Parse()
+	fmt.Printf("using the story in %s.\n", *fileName)
+
+	f, err := os.Open(*fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", *port), h))
+}
